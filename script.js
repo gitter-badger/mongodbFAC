e9e863b4254dbb2a30da8a46c0e1c5a2d6824f7f
Danielwhyte <danwhy@gmail.com>
2015-06-09 14:26:00 +0100

    more js & html

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var search = $('#search'),
-    facName = $('facName');
+    create = $('#create'),
+    fact = $('#fact'),
+    facName = $('#facName'),
+    addUser = $('#addUser');
 
 search.click(function() {
     var name = search.val();
-        $.get( '/', name, function(data) {
-        console.log(data);
+    $.get( '/read&' + name, function(data) {
+        if (!data.length){
+            addUser.removeClass('hidden');
+            search.addClass('hidden');
+        } else {
+            console.log(data);
+        }
+    });
+});
+
+create.click(function() {
+    var user = {name : search.val(), fact : fact.val()};
+    $.post('/create', user, function(data) {
+        console.log('User Created');
     });
 });
